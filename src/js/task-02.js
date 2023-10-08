bef842f912db81b511fa10703d3a852751fe1581
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-08 18:40:57 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(element => {
+  const listsEl = document.createElement('li');
+  listsEl.textContent = element;
+  listsEl.classList.add('item');
+
+  return listsEl
+});
+
+ingredientsContainerEl.append(...elements);
