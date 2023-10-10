1042475e3fdff3d11571eec748fe30d09ddaf0e9
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-10 22:35:47 +0300

    Update task-05

---
+++ src/js/task-05.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', onInputChenge);
+
+function onInputChenge(event) {
+  outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === '') {
+    outputEl.textContent = 'Anonymous';
+  }
+}
